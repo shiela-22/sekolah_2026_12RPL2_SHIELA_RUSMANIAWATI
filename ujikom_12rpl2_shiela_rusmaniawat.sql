cd2912dbe407c7ea292a3eb25cb35932438e23ab
shiela-22 <cilakomputer@gmail.com>
2026-02-15 00:04:39 -0800

    Add files via upload

---
+++ ujikom_12rpl2_shiela_rusmaniawat.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 15, 2026 at 08:03 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ujikom_12rpl2_shiela_rusmaniawat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `input_aspirasi`
+--
+
+CREATE TABLE `input_aspirasi` (
+  `id_pelaporan` int NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `id_kategori` int NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `ket` varchar(50) NOT NULL,
+  `status` enum('Menunggu','proses','selesai') CHARACTER SET macce COLLATE macce_bin NOT NULL,
+  `feedback` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `input_aspirasi`
+--
+
+INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `ket`, `status`, `feedback`) VALUES
+(1, '04', 1, 'taman ', 'pohon tumbang', 'Menunggu', NULL),
+(2, '04', 2, 'masjid', 'atap bocor', 'Menunggu', NULL),
+(3, '03', 2, 'kelas', 'ac mati', 'Menunggu', NULL),
+(4, '03', 2, 'kelas', 'ac mati', 'Menunggu', NULL),
+(5, '03', 2, 'kelas', 'ac mati', 'Menunggu', NULL),
+(6, '03', 2, 'kelas', 'ac mati', 'Menunggu', NULL),
+(7, '03', 2, 'kelas', 'ac mati', 'Menunggu', NULL),
+(8, '02', 1, 'lapangan', 'lapangan nya retak', 'Menunggu', NULL),
+(9, '02', 1, 'lapangan', 'lapangan nya retak', 'Menunggu', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int NOT NULL,
+  `ket_kategori` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `ket_kategori`) VALUES
+(1, 'Lingkungan'),
+(2, 'Fasilitas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('siswa','admin') NOT NULL DEFAULT 'siswa',
+  `nis` varchar(10) DEFAULT NULL,
+  `kelas` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`, `nis`, `kelas`) VALUES
+(1, 'cila', 'cilaimupp26', 'siswa', '123', '12 rpl 2'),
+(2, 'sila', 'silacanci', 'siswa', '2233', '10 plg 2'),
+(3, 'shiela', 'shielakiyowok', 'siswa', '3344', '11 tkj 2'),
+(4, 'nunu', '$2y$10$0y7c5k.GiVCAsB9QzYWBjeCwrK40J1tbcPXim/3V8ItiNJBkN5.EK', 'siswa', '04', 'XII TP 2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD PRIMARY KEY (`id_pelaporan`),
+  ADD KEY `nis` (`nis`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nis` (`nis`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  MODIFY `id_pelaporan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
